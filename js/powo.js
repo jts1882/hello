ab3d5c484d14de11b8b5e5d1227370b655b3a1b9
jts1882 <68289031+jts1882@users.noreply.github.com>
2021-10-04 14:59:01 +0100

    Update powo.js

--- js/powo.js
+++ js/powo.js
@@ -3,5 +3,5 @@ function initialise() {
   var output = "initialise test programme";
   alert(output);   
   $('#content1').html(output);
-  alert(output);   
+  alert( $('#content1').html() );   
 }
