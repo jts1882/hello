284feaeefd0dcad9999370f2243d755491841402
jts1882 <68289031+jts1882@users.noreply.github.com>
2021-10-04 15:44:26 +0100

    Update powo.js

--- js/powo.js
+++ js/powo.js
@@ -1,7 +1,28 @@
 function initialise() {
      
   var output = "initialise test programme";
-  alert(output);   
+  //alert(output);   
   $('#content1').html(output);
-  alert( $('#content1').html() );   
+  
+  
+}
+function loadPython() {          
+   
+   var url = "powo.py";
+   url = "testpython.py";
+   url = "testPykew.py";
+   
+   $.ajax({
+       url: url,
+       type: 'GET',
+       data: { param: "text"},
+       success: function (response) {
+           console.log(response);
+           var html = '<iframe id="TestiFrame" title="Test iFrame">' +response +'</iframe>'; 
+           $("#content").html(html);
+       },
+       error: function (error) {
+           console.log(error);
+       }
+    });
 }
